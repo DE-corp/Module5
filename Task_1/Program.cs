17798257a2e36c4267ea877e1b7f0b7d9fb9f1ae
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-22 16:07:46 +0300

    Добавьте файлы проекта.

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string Name, string[] Dishes) User;
+
+            Console.Write("Введи свое имя: ");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            for(var i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.Write("Введи свое любимое блюдо №{0}: ", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
+
+
+            Console.Clear();
+            Console.WriteLine("Ваше имя: {0}", User.Name);
+            
+            Console.WriteLine("Список ваших любимых блюд:\n");
+            foreach (var item in User.Dishes)
+            {
+                Console.WriteLine(item);
+            }
+     
+            Console.ReadLine();
+        }
+    }
+}
