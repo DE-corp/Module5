119769dab9a56158bb9c0b2b8f8b6285efd0f866
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-22 18:07:20 +0300

    Добавил Задание 5.2.8

---
+++ Task_5.2.8/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task_5._2._8
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (var i = 0; i < result.Length; i++)
+            {
+                Console.Write("Введи элемент массива №{0}: ", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            var sorted_array = SortArray(result);
+            return sorted_array;
+        }
+        static int[] SortArray(int[] array)
+        {
+            int temp = 0;
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            foreach (var item in GetArrayFromConsole())
+            {
+                Console.Write(item + " ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
