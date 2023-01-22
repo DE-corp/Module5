c19e5f31463524fc5811b3ba478dc24dee751467
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-22 17:06:32 +0300

    Добавлено Задание 5.1.6

---
+++ Task_5.1.6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Task_5._1._6
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for(var i = 0; i < result.Length; i++)
+            {
+                Console.Write("Введи элемент массива №{0}: ", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            int temp = 0;
+            for (var i = 0; i < result.Length; i++)
+            {
+                for (var j = i + 1; j < result.Length; j++)
+                {
+                    if (result[i] > result[j])
+                    {
+                        temp = result[i];
+                        result[i] = result[j];
+                        result[j] = temp;
+                    }
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            foreach(var item in GetArrayFromConsole())
+            {
+                Console.Write(item + " ");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
