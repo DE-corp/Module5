86a4971d83d31d2f73428a3c317c4be8f56d49da
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-23 20:07:19 +0300

    Добавил Задание 5.3.3

---
+++ Task_5.3.3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_5._3._3
+{
+    class Program
+    {
+        static void ChangeName(ref string name)
+        {
+            Console.Write("Введите имя: ");
+            name = Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            var SomeName = "Евгения";
+            Console.WriteLine(SomeName);
+
+            ChangeName(ref SomeName);
+            Console.WriteLine(SomeName);
+
+            Console.ReadLine();
+        }
+    }
+}
