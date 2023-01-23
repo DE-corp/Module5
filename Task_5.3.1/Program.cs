4f457b4aff5f5476bff32c93d06c53a35d763a4e
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-23 19:29:50 +0300

    Добавил Задание 5.3.1

---
+++ Task_5.3.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_5._3._1
+{
+    class Program
+    {
+        static void ChangeAge(string age)
+        {
+            Console.Write("Введите возраст: ");
+            age = Console.ReadLine();
+
+        }
+        static void Main(string[] args)
+        {
+            var SomeAge = "31";
+            Console.WriteLine(SomeAge);
+
+            ChangeAge(SomeAge);
+            Console.WriteLine(SomeAge);
+
+            Console.ReadLine();
+        }
+    }
+}
