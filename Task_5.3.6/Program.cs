f9dd0745591228e765e3b0c40fb5142d6d66f22f
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-24 19:14:37 +0300

    Добавил Задание 5.3.6

---
+++ Task_5.3.6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_5._3._6
+{
+    class Program
+    {
+        static void BigDataOperation(in int[] arr)
+        {
+            arr[0] = 4;
+        }
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3 };
+            BigDataOperation(arr);
+
+            Console.WriteLine(arr[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
