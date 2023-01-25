a44feee148b17ee0caca71a9bf27460f62ec3b6d
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-25 18:58:47 +0300

    Добавил Задание 5.5.8

---
+++ Task_5.5.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_5._5._8
+{
+    class Program
+    {
+        private static int PowerUp(int N, byte pow)
+        {
+			if (pow == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				if (pow == 1)
+				{
+					return N;
+				}
+				else
+				{
+					return N * PowerUp(N, --pow);
+				}
+			}
+		}
+        static void Main(string[] args)
+        {
+            Console.WriteLine(PowerUp(2, 4));
+        }
+    }
+}
