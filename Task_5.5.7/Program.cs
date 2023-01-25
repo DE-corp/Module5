02c917da901ca4761e29a42639fdf466344052e4
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-25 18:53:58 +0300

    Добавил Задание 5.5.7

---
+++ Task_5.5.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_5._5._7
+{
+    class Program
+    {
+        static decimal Factorial(int x)
+        {
+            if (x == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * Factorial(x - 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Factorial(20));
+            Console.ReadLine();
+        }
+    }
+}
